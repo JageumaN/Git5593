ed949f319843753afc2487573d2eeda141d25cee
JageumaN <wkrmaks0137@gmail.com>
2026-04-29 14:30:08 +0900

    Add prime numbers

--- mynumber.c
+++ mynumber.c
@@ -4,5 +4,15 @@ int main() {
     printf("\n");
     for(int i=2; i<=100; i+=2) printf("%d ", i);
     printf("\n");
+    
+    // 소수(Prime) 출력
+    for(int i=2; i<=100; i++) {
+        int is_prime = 1;
+        for(int j=2; j*j<=i; j++) {
+            if(i%j == 0) { is_prime = 0; break; }
+        }
+        if(is_prime) printf("%d ", i);
+    }
+    printf("\n");
     return 0;
 }
